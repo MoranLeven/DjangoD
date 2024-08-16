2406ba23bdb74278f69b6aafa7e7def1f3c0818a
morana <morana@opentext.com>
2024-08-16 20:12:08 +0530

    added env variable

--- messageboard/settings.py
+++ messageboard/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ie!*)toj9@eg211a(!(dmp&u7=*si3-sioy8c%m+4)372l%)r#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", 'False').lower() == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
