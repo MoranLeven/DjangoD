8b0d3f217e506307f9681f4751c682a70206714a
morana <morana@opentext.com>
2024-08-16 20:23:06 +0530

    made some minor changes in paths

--- msgboard/urls.py
+++ msgboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView
+from .views import home
 urlpatterns = [
-path('', HomePageView.as_view(), name='home'),
+path('', home),
 ]
--- msgboard/views.py
+++ msgboard/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import ListView 
+from django.views.generic import ListView, CreateView
 from .models import msgboard
-class HomePageView(ListView):
-    model = msgboard
-    template_name = 'home.html'
-    context_object_name='all_msgboard_list'
+from django.http import HttpResponse
+# class HomePageView(ListView):
+#     model = msgboard
+#     template_name = 'home.html'
+#     context_object_name='all_msgboard_list'
+
+
+def home(request):
+    return HttpResponse("Hey I'm up and running hurray checkout my another project on crud <a href='https://djangodeploy-6i7w.onrender.com/crud'>CRUD APP</a>")
